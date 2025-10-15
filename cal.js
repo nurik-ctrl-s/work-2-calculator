685be7fecfa77d022200bb9c89a3ca914ceb7634
nurik-ctrl-s <nseidahmatov@gmail.com>
2025-10-16 01:51:53 +0600

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,14 @@
+let num1 = 1;
+let num2 = 5;
+let opperator = "+";
+if (opperator == "+") {
+  console.log("sum :", num1 + num2);
+} else if (opperator == "-") {
+  console.log("sum :", num1 + num2);
+} else if (opperator == "*") {
+  console.log("sum :", num1 * num2);
+} else if (opperator == "/") {
+  console.log("sum :", num1 / num2);
+} else {
+  console.log("Invalid Input");
+}
